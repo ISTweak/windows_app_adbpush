0f7fcc4f7cecd641c45790ba9b8d7fecca78c626
ISTweak <istweak@d-o-b.org>
2013-06-05 20:49:05 +0900

    取りあえず追加

---
+++ Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(578, 44)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(522, 14)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox1.TabIndex = 9
+        Me.CheckBox1.Text = "10秒"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(12, 12)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(106, 20)
+        Me.ComboBox3.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(383, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "権限"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(124, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 12)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "送り先"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(418, 13)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(98, 20)
+        Me.ComboBox2.TabIndex = 5
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(167, 13)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(210, 20)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 44)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.GroupBox3)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.GroupBox2)
+        Me.SplitContainer1.Size = New System.Drawing.Size(578, 428)
+        Me.SplitContainer1.SplitterDistance = 130
+        Me.SplitContainer1.TabIndex = 6
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.TextBox2)
+        Me.GroupBox3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox3.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(578, 130)
+        Me.GroupBox3.TabIndex = 0
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "コマンド"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Black
+        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.TextBox2.Location = New System.Drawing.Point(3, 15)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(572, 112)
+        Me.TextBox2.TabIndex = 0
+        Me.TextBox2.WordWrap = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox2.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(578, 294)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "ログ"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(3, 15)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(572, 276)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.WordWrap = False
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(578, 472)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AdbPush"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,193 @@
+﻿Imports System.IO
+Imports System.Diagnostics
+
+Public Class Form1
+    Private getFiles() As String
+
+    Private PSI As New ProcessStartInfo()
+    Private P As New Process
+
+    Private Const cmdPush As String = "/c adb push ""{0}"" {1} "
+    Private Const cmdChmod As String = " && adb shell chmod {0} {1}/{2}"
+
+    Private Sub Form1_DragDrop(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles Me.DragDrop
+        getFiles = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+        Me.Cursor = Cursors.WaitCursor
+
+        Select Case ComboBox3.SelectedIndex
+            Case 0
+                adbpush()
+            Case 1
+                install()
+            Case 2
+                install("-r ")
+        End Select
+
+        Me.Cursor = Cursors.Default
+    End Sub
+
+    Private Sub Form1_DragEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) = False Then
+            Return
+        End If
+
+        Dim FilePath() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+        For idx As Integer = 0 To FilePath.Length - 1
+            If Not File.Exists(FilePath(idx)) Then
+                Return
+            End If
+
+            If ComboBox3.SelectedIndex <> 0 Then
+                If String.Compare(Path.GetExtension(FilePath(idx)), ".apk", True) <> 0 Then
+                    Return
+                End If
+            End If
+        Next idx
+
+        e.Effect = DragDropEffects.Copy
+    End Sub
+
+    Private Sub SetPushDirCMB()
+        With ComboBox1.Items
+            .Clear()
+            .Add("/data/local/tmp")
+            .Add("/data/local")
+            .Add("/data/root")
+
+            .Add("/system/app")
+            .Add("/system/etc")
+            .Add("/system/lib")
+            .Add("/system/framework")
+            .Add("/system/xbin")
+            .Add("/system/vendor")
+            .Add("/system/usr")
+            .Add("/system/lib/hw")
+            .Add("/system/lib/egl")
+        End With
+        ComboBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub SetAnyCMB()
+        With ComboBox2.Items
+            .Clear()
+            .Add("0777")
+            .Add("0666")
+            .Add("0755")
+            .Add("0644")
+        End With
+
+        With ComboBox3.Items
+            .Clear()
+            .Add("ファイル転送")
+            .Add("インストール")
+            .Add("アップデート")
+        End With
+
+        ComboBox2.SelectedIndex = 0
+        ComboBox3.SelectedIndex = 0
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim ArgCmd As String = Microsoft.VisualBasic.Command()
+
+        SetPushDirCMB()
+        SetAnyCMB()
+
+        InitPSI()
+
+        runCommand("/c adb start-server", False)
+
+        If ArgCmd.Length > 0 Then
+            ArgCmd = ArgCmd.Replace("""", "")
+            If File.Exists(ArgCmd) Then
+                getFiles = {ArgCmd}
+                adbpush()
+            End If
+        End If
+    End Sub
+
+    Private Sub InitPSI()
+        With PSI
+            .FileName = System.Environment.GetEnvironmentVariable("ComSpec")
+            .RedirectStandardInput = False
+            .RedirectStandardOutput = True
+            .UseShellExecute = False
+            .CreateNoWindow = True
+        End With
+    End Sub
+
+    Private Sub adbpush()
+        If ComboBox1.Text = "" Then
+            MessageBox.Show("push先どこ？")
+            Return
+        End If
+
+        Dim cmd As String
+        For idx As Integer = 0 To getFiles.Length - 1
+            cmd = String.Format(cmdPush, getFiles(idx), ComboBox1.Text)
+            If ComboBox2.Text <> "" Then
+                cmd &= String.Format(cmdChmod, ComboBox2.Text, ComboBox1.Text, Path.GetFileName(getFiles(idx)))
+            End If
+
+            TextBox2.Text &= cmd & vbNewLine
+
+            If runCommand(cmd) Then
+                TextBox1.Text &= Path.GetFileName(getFiles(idx)) & "を転送しました。" & vbNewLine
+            End If
+        Next
+    End Sub
+
+    Private Sub install(Optional ByVal m As String = "")
+        Dim cmd As String
+        For idx As Integer = 0 To getFiles.Length - 1
+            cmd = "/c adb install " + m + """" + getFiles(idx) + """"
+            runCommand(cmd)
+        Next
+    End Sub
+
+    Private Function runCommand(Cmd As String, Optional log As Boolean = True) As Boolean
+        PSI.Arguments = Cmd
+
+        P = Process.Start(PSI)
+        With P
+            If log Then
+                TextBox1.Text &= .StandardOutput.ReadToEnd
+            End If
+
+            If CheckBox1.Checked Then
+                .WaitForExit(10000)
+            Else
+                .WaitForExit()
+            End If
+
+            If .HasExited = False Then
+                If .Responding Then
+                    .Close()
+                Else
+                    .Kill()
+                End If
+
+                TextBox1.Text &= "タイムアウトしました。"
+                Return False
+            End If
+        End With
+
+        Return True
+    End Function
+
+    Private Sub ComboBox3_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox3.SelectedIndexChanged
+        Dim b As Boolean = (ComboBox3.SelectedIndex = 0)
+        ComboBox1.Enabled = b
+        ComboBox2.Enabled = b
+    End Sub
+
+    Private Sub TextBox1_DoubleClick(sender As Object, e As System.EventArgs) Handles TextBox1.DoubleClick
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub TextBox2_DoubleClick(sender As Object, e As System.EventArgs) Handles TextBox2.DoubleClick
+        TextBox2.Text = ""
+    End Sub
+End Class
